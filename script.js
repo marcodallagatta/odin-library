0170472da6c40df3b4e4265e7e3025227e2a7e68
marcodallagatta <36917918+marcodallagatta@users.noreply.github.com>
2022-05-12 23:18:02 +0200

    add and display new books

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 	plugins: ['jsdom-quokka-plugin'],
  })
 
+// DOM Elements
 const bookshelf = document.getElementById('bookshelf');
+const newBookButton = document.getElementById('newBook');
 
+// Library Management
 let myLibrary = [];
 
 function Book (name, author, read) {
@@ -17,15 +20,33 @@ function addBookToLibrary(book) {
 	myLibrary.push(currentBook);
 }
 
+function newBook() {
+	let title = prompt('Title:');
+	let author = prompt('Author:');
+	let read = prompt('Have you finished it? (Y/N)');
+	if (read.toLowerCase() === 'y') {
+		read = true;
+	 } else {
+		read = false;
+	}
+	const newBook = new Book(title, author, read);
+	addBookToLibrary(newBook);
+}
+
+// Sample Data
 const Hobbit = new Book('The Hobbit', 'Tolkien', false);
 const Steppen = new Book('Steppenwolf', 'H. Hesse', true);
-
 addBookToLibrary(Hobbit);
 addBookToLibrary(Steppen);
 
-console.log(myLibrary);
-
-function showBooks() {
+// DOM Bookshelf
+function renderBookshelf() {
+	// clear bookshelf, unless header
+	document.querySelectorAll('tr').forEach((i) => {
+		if (i.id  != 'header') {
+			i.remove()
+		}
+	})
 	myLibrary.forEach((book) => {
 		let row = bookshelf.insertRow(-1);
 		let cellA = row.insertCell(0);
@@ -36,5 +57,10 @@ function showBooks() {
 		cellC.innerText = book[2];
 	})
 }
+renderBookshelf();
 
-showBooks();
+// Event Listeners
+newBookButton.addEventListener('click', function() {
+	newBook();
+	renderBookshelf();
+});
