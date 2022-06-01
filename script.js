2892514f7afa9e573c1f933ca8a3591446804baa
marcodallagatta <36917918+marcodallagatta@users.noreply.github.com>
2022-06-01 22:01:52 +0200

    add grid visualization

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 ({plugins:['jsdom-quokka-plugin']})
-console.clear();
 
 // DOM Elements
-const bookshelf = document.getElementById('bookshelf');
+const bookshelf = document.querySelector('.bookshelf');
 const newBookButton = document.getElementById('newBook');
 
 // Library Management
@@ -28,15 +27,12 @@ function Book (title, author, read = false, rating = 0, pages = 0, pubYear = 0) 
 	}
 	this.removeBook = function () {
 		myLibrary.splice(this.id, 1);
-		console.log(`deleting id ${this.id}...`)
-		console.table(myLibrary);
 		renderBookshelf();
 	}
 }
 
 function addBookToLibrary(book) {
 	myLibrary.push(book);
-	console.table(myLibrary);
 }
 
 function newBook() {
@@ -58,55 +54,27 @@ function addSampleData() {
 	addBookToLibrary(new Book('Steppenwolf', 'H. Hesse', true, 3));
 	addBookToLibrary(new Book('The Way of Kings', 'Sandor Branderson', true, 5));
 	addBookToLibrary(new Book('The Graveyard Book', 'Neil Gaiman', true, 5));
-	console.table(myLibrary);
 	renderBookshelf();
 }
 
 function clearBookshelf() {
 	myLibrary = [];
-	console.table(myLibrary);
+	bookshelf.innerHTML = '';
 	renderBookshelf();
 }
 
 
 // DOM Bookshelf
 function renderBookshelf() {
-	// clear bookshelf, unless header
-	document.querySelectorAll('tr').forEach((i) => {
-		if (i.id  != 'header') {
-			i.remove()
-		}
-	})
-	let bookIndex = 0;
 	myLibrary.forEach(book => {
-		book[0] = bookIndex++;
-		book.id = book[0];
-		let row = bookshelf.insertRow(-1);
-		let cellA = row.insertCell(0);
-		let cellB = row.insertCell(1);
-		let cellC = row.insertCell(2);
-		let cellD = row.insertCell(3);
-		let cellE = row.insertCell(4);
-		let cellF = row.insertCell(5);
-		let cellG = row.insertCell(6);
-		cellA.innerHTML = book.id;
-		cellB.innerHTML = `<span onclick='myLibrary[${book.id}].removeBook()'>x</span> ${book.title}`;
-		cellC.innerHTML = book.author;
-		cellD.innerHTML = `<span onclick='myLibrary[${book.id}].toggleRead()'>${book.read}</span>`;
-		cellE.innerHTML = (book.rating);
-		cellF.innerHTML = (book.pages);
-		cellG.innerHTML = (book.pubYear);
+		let bookDiv = document.createElement('div');
+		bookDiv.classList.add('book');
+		const randomColor = Math.floor(Math.random()*16777215).toString(16);
+		bookDiv.style.backgroundColor = "#" + randomColor;
+		bookDiv.innerHTML = `<h3>${book.title}</h3><h4>${book.author}</h4>`;
+		bookshelf.appendChild(bookDiv);
 	})
 }
-
-// this.id = idInteger++;
-// this.title = title;
-// this.author = author;
-// this.read = read;
-// this.rating = rating;
-// this.pages = pages;
-// this.pubYear = pubYear;
-
 addSampleData();
 renderBookshelf();
 
